36f8b728ee1752146f12399f929098139702a258
Andelo Jakolis <andelo.jakolis@playhera.com>
2023-11-10 13:51:00 +0100

    first commit

---
+++ client/src/components/FundCard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import { tagType, thirdweb } from '../assets';
+import { daysLeft } from '../utils';
+
+const FundCard = ({ owner, title, description, target, deadline, amountCollected, image, handleClick }) => {
+  const remainingDays = daysLeft(deadline);
+    
+  return (
+    <div className=''> {/** 2:28 */}
+
+    </div>
+  )
+}
+
+export default FundCard
